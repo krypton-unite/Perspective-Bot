8bf0e086023cdbbbbc8cd4353c8ae3aa6a796c0b
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-08-18 07:50:04 +0100

    :bug: Should now fix it.

--- src/discord.js
+++ src/discord.js
@@ -169,7 +169,7 @@ mongo_client.connect_mongo_client((err, db_client) => {
       }
       if (robot_creator == null){
         robot_creator = message.author.id
-        message.channel.send(`Meu criador é você, <@${userid}>!`);
+        message.channel.send(`Meu criador é você, <@${robot_creator}>!`);
       }
       if (message.author.id == robot_creator){
         offence_records.deleteMany( { offending_user: message.author.id } )
